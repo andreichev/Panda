2274f1cd48bded683c103125480931ee9839e1c1
Andreichev Michail <andreichev.m@mail.ru>
2022-11-01 15:33:08 +0300

    allocation alignment

--- Foundation/include/Foundation/Allocator.hpp
+++ Foundation/include/Foundation/Allocator.hpp
@@ -2,17 +2,26 @@
 
 #include <cstdlib>
 
-#if DEBUG
-#    define ALLOC(allocator, size) Foundation::alloc(allocator, _size, __FILE__, __LINE__)
-#    define FREE(allocator, ptr) Foundation::free(allocator, ptr, __FILE__, __LINE__)
+#if ALLOCATOR_DEBUG
+#    define ALLOC(allocator, size) Foundation::alloc(allocator, _size, 0, __FILE__, __LINE__)
+#    define FREE(allocator, ptr) Foundation::free(allocator, ptr, 0, __FILE__, __LINE__)
 #    define DELETE(_allocator, _ptr) Foundation::deleteObject(_allocator, _ptr)
+
+#    define ALIGNED_ALLOC(_allocator, _size, _align) Foundation::alloc(_allocator, _size, _align, __FILE__, __LINE__)
+#    define ALIGNED_FREE(_allocator, _ptr, _align) Foundation::free(_allocator, _ptr, _align, __FILE__, __LINE__)
+#    define ALIGNED_DELETE(_allocator, _ptr, _align) Foundation::deleteObject(_allocator, _ptr, _align, __FILE__, __LINE__)
 #else
-#    define ALLOC(allocator, size) Foundation::alloc(allocator, size)
-#    define FREE(allocator, ptr) Foundation::free(allocator, ptr)
-#    define DELETE(_allocator, _ptr) Foundation::deleteObject(_allocator, _ptr, __FILE__, __LINE__)
+#    define ALLOC(allocator, size) Foundation::alloc(allocator, size, 0)
+#    define FREE(allocator, ptr) Foundation::free(allocator, ptr, 0)
+#    define DELETE(_allocator, _ptr) Foundation::deleteObject(_allocator, _ptr, 0, __FILE__, __LINE__)
+
+#    define ALIGNED_ALLOC(_allocator, _size, _align) Foundation::alloc(_allocator, _size, _align)
+#    define ALIGNED_FREE(_allocator, _ptr, _align) Foundation::free(_allocator, _ptr, _align)
+#    define ALIGNED_DELETE(_allocator, _ptr, _align) Foundation::deleteObject(_allocator, _ptr, _align)
 #endif
 
 #define NEW(_allocator, _type) PLACEMENT_NEW(ALLOC(_allocator, sizeof(_type)), _type)
+#define ALIGNED_NEW(_allocator, _type, _align) PLACEMENT_NEW(ALIGNED_ALLOC(_allocator, sizeof(_type), _align), _type)
 #define PLACEMENT_NEW(_ptr, _type) ::new (Foundation::PlacementNewTag(), _ptr) _type
 
 namespace Foundation {
@@ -27,7 +36,7 @@ namespace Foundation {
 struct AllocatorI {
     virtual ~AllocatorI() = default;
 
-    virtual void *realloc(void *ptr, size_t size, const char *file, uint32_t line) = 0;
+    virtual void *realloc(void *ptr, size_t size, size_t _align, const char *file, uint32_t line) = 0;
 };
 
 class DefaultAllocator : public AllocatorI {
@@ -36,12 +45,18 @@ public:
 
     ~DefaultAllocator() override;
 
-    void *realloc(void *ptr, size_t size, const char *file, uint32_t line) override;
+    void *realloc(void *ptr, size_t size, size_t _align, const char *file, uint32_t line) override;
 };
 
-void *alloc(AllocatorI *allocator, size_t size, const char *file = NULL, uint32_t line = 0);
+template<typename Ty>
+Ty alignUp(Ty _a, int32_t _align);
 
-void free(AllocatorI *allocator, void *ptr, const char *file = NULL, uint32_t line = 0);
+void *alloc(AllocatorI *allocator, size_t size, size_t _align = 0, const char *file = nullptr, uint32_t line = 0);
+
+void free(AllocatorI *allocator, void *ptr, size_t _align = 0, const char *file = nullptr, uint32_t line = 0);
+
+template<typename ObjectT>
+void deleteObject(AllocatorI *_allocator, ObjectT *_object, size_t _align = 0, const char *_file = nullptr, uint32_t _line = 0);
 
 AllocatorI *getAllocator();
 
